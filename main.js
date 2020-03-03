9bfa5f3d2591fb5d6040869ec24d50b6449b21d0
gimo <giulianocarabajal48@gmail.com>
2020-03-02 22:05:19 -0300

    Algunas cosas para que quede mejor el login

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-var mensaje = "3 cerditos";
-console.log(mensaje);
+function mensaje() {
+  var saludo = "hola amigo";
+  var antisaludo = "chau amigo tas re tonto";
+  var qcyo = true;
+  if (qcyo === false) {
+    console.log(saludo);
+  } else {
+    console.log(antisaludo);
+  }
+}
+mensaje();
