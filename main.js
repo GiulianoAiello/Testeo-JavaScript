b1481c6a3d2cd03842d18eb1148ab6d52a1b7b72
gimo <giulianocarabajal48@gmail.com>
2020-03-09 19:15:49 -0300

    varios testeos en js aunque la mayoría están en comentarios o han sido borrados

--- main.js
+++ main.js
@@ -36,8 +36,6 @@ document.querySelector("body").addEventListener("click", cambiaColor);
 function saludar(mensaje, nombre) {
   console.log(mensaje + nombre);
 }
-*/
-
 function recogeDatos(evento) {
   event.preventDefault();
   var nombre = document.querySelector("#nombre").value;
@@ -46,4 +44,27 @@ function recogeDatos(evento) {
   console.log("hola, " + nombre + ", tu edad es de " + edad + " años");
 }
 var miForm = document.querySelector("#formulario");
-miForm.addEventListener("submit", recogeDatos);
+miForm.a/*al crear una variable refiriéndose a la parte del documento html formulario
+nos ahorramos seleccionar dicha parte en un event listener
+function cambiaColor() {
+  this.classList.toggle("formulariovich");
+}
+document.querySelector("form").addEventListener("click", cambiaColor);
+*/
+function recogeDatos(evento) {
+  evento.preventDefault();
+  var nombre = document.querySelector("#nombre").value;
+  var fecha = document.querySelector("#fecha").value;
+  var edad = 2020 - fecha;
+  console.log("Hola, " + nombre + " tu edad es de " + edad + " años");
+}
+var myForm = document.querySelector("#formulario");
+myForm.addEventListener("submit", recogeDatos);
+
+var encabezado = document.querySelector("h1");
+encabezado.textContent = "de pana!!!";
+
+function saludar(mensaje, nombre) {
+  console.log(mensaje + nombre);
+}
+saludar("de pana", " de panasea");
